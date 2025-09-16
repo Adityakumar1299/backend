2bde146663ffdcffea7b3cfafd0bc41572a548c5
Adityakumar1299 <aditya1239kumar@gmail.com>
2025-09-16 10:48:50 +0530

    main

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants";
+import connectDB from "./db";
+
+
+
+
+
+
+connectDB()
+
+// import express from "express";
+// const app = express();
+
+// (async () => {
+//   try {
+//     mongoose.connect(`$/{process.env.MONGODB_URI}/${DB_NAME}`);
+//     app.on("error", (error) => {
+//       console.log("ERR: ", error);
+//       throw error;
+//     });
+
+//     app.listen(process.env.PORT, () => {
+//       console.log(`App is listening on port $ {process.env.PORT}`);
+//     });
+//   } catch (error) {
+//     console.error("Error connecting to MongoDB", error);
+//     throw error;
+//   }
+// })();
