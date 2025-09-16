4dcd89c69d9c929b40d51dd79dcf4232a6d5853e
Adityakumar1299 <aditya1239kumar@gmail.com>
2025-09-16 12:06:43 +0530

    connect to DB

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
-import mongoose from "mongoose";
-import { DB_NAME } from "./constants";
-import connectDB from "./db";
+// import mongoose from "mongoose";
+// import { DB_NAME } from "./constants";
+import connectDB from "./db/index.js";
+import dotenv from "dotenv"
 
-
+dotenv.config({
+    path:'./env'
+})
 
 
 
